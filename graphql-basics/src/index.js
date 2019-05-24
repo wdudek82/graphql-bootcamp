f4e8074189087f0513e24a437c958fd4547fae5f
Wojciech Dudek <wdudek82@gmail.com>
2019-05-24 23:37:25 +0200

    Added Comment type and its relations/resolvers with User and Post

--- graphql-basics/src/index.js
+++ graphql-basics/src/index.js
@@ -7,9 +7,26 @@ const typeDefs = `
     name: String!
     email: String!
     age: Int
-    posts: [Post]
+    posts: [Post]!
+    comments: [Comment]!
+  }
+
+  type Post {
+    id: ID!
+    author: User!
+    title: String!
+    body: String!
+    published: Boolean!
+    comments: [Comment]!
   }
   
+  type Comment {
+    id: ID!
+    text: String!
+    author: User!
+    post: Post!
+  }
+
   type Product {
     id: ID!
     title: String!
@@ -18,25 +35,13 @@ const typeDefs = `
     rating: Float
     inStock: Boolean!
   }
-  
-  type Post {
-    id: ID!
-    author: String!
-    title: String!
-    body: String!
-    published: Boolean!
-  }
 
   type Query {
-    greeting(name: String): String!
-    add(numbers: [Float!]!): Float!
-    name: String!
-    age: Int!
-    employed: Boolean!
-    gpa: Float
-    user: User!
+    users(name: String): [User!]!
+    user(id: ID!): User
+    posts(query: String): [Post!]!
+    comments: [Comment!]!
     product: Product!
-    posts: [Post!]
     userPosts(author: String!): [Post!]
   }
 `;
@@ -44,64 +49,92 @@ const typeDefs = `
 const postList = [
   {
     id: '1',
-    author: 'Neevor',
+    author: '1',
     title: 'New post 1',
     body: 'This is new post 1',
     published: true,
   },
   {
     id: '2',
-    author: 'Foo',
+    author: '2',
     title: 'New post 2',
-    body: 'This is new post 2',
+    body: 'This is a test 1',
     published: false,
   },
   {
     id: '3',
-    author: 'Neevor',
+    author: '1',
     title: 'New post 3',
     body: 'This is new post 3',
     published: false,
   },
 ];
 
+const userList = [
+  {
+    id: '1',
+    name: 'Neevor',
+    email: 'neevor@testing.com',
+    age: 240,
+  },
+  {
+    id: '2',
+    name: 'Jon Doe',
+    email: 'jon.doe@testing.com',
+    age: 28,
+  },
+  {
+    id: '3',
+    name: 'Jane Doe',
+    email: 'jane.doe@testing.com',
+    age: 24,
+  },
+];
+
+const commentList = [
+  {
+    id: '1',
+    text: 'Comment 1A',
+    author: '1',
+    post: '1',
+  },
+  {
+    id: '1',
+    text: 'Comment 1B',
+    author: '2',
+    post: '1',
+  },
+  {
+    id: '2',
+    text: 'Comment 2',
+    author: '3',
+    post: '2',
+  },
+  {
+    id: '3',
+    text: 'Comment 3',
+    author: '1',
+    post: '3',
+  },
+];
+
 // Resolvers
 const resolvers = {
   Query: {
-    greeting(parent, args, context, info) {
-      const { name } = args;
+    users(parent, args, context, info) {
+      const hasName = Object.prototype.hasOwnProperty.call(args, 'name');
 
-      console.log('Parent:', parent);
-      console.log('Args:', args);
-      console.log('Context:', context);
-      console.log('Info:', info);
+      if (hasName) {
+        return userList.filter((user) => {
+          return user.name.toLowerCase() === args.name.toLowerCase();
+        });
+      }
+      return userList;
+    },
+    user(parent, args, context, info) {
+      const { id } = args;
 
-      return `Hello ${name || 'World'}`;
-    },
-    add(_, args) {
-      return args.numbers.reduce((acc, val) => acc + val, 0);
-    },
-    name() {
-      return 'Paulina';
-    },
-    age() {
-      return 280;
-    },
-    employed() {
-      return true;
-    },
-    gpa() {
-      return null;
-    },
-    user() {
-      return {
-        id: '12',
-        name: 'Neevor',
-        email: 'neevor@testing.com',
-        age: 120,
-        employed: true,
-        posts: postList,
-      };
+      return userList.find((user) => user.id === id);
     },
     product() {
       return {
@@ -113,12 +146,50 @@ const resolvers = {
         inStock: true,
       };
     },
-    posts() {
-      return postList;
+    posts(parent, args, context, info) {
+      const hasQuery = Object.prototype.hasOwnProperty.call(args, 'query');
+
+      if (!hasQuery) return postList;
+
+      return postList.filter(({ title, body }) => {
+        const lowerTitle = title.toLowerCase();
+        const lowerBody = body.toLowerCase();
+        const lowerQuery = args.query.toLowerCase();
+
+        return (
+          lowerTitle.includes(lowerQuery) || lowerBody.includes(lowerQuery)
+        );
+      });
+    },
+    comments(parent, args, context, info) {
+      return commentList;
     },
     userPosts(_, { author }) {
-      console.log(author);
       return postList.filter((post) => post.author === author);
+    },
+  },
+  Post: {
+    author(parent, args, context, info) {
+      return userList.find((user) => user.id === parent.author);
+    },
+    comments(parent, args, context, info) {
+      return commentList.filter((comment) => parent.id === comment.post);
+    },
+  },
+  User: {
+    posts(parent, args, context, info) {
+      return postList.filter((post) => parent.id === post.author);
+    },
+    comments(parent, args, context, info) {
+      return commentList.filter((comment) => parent.id === comment.author);
+    },
+  },
+  Comment: {
+    post(parent, arts, context, info) {
+      return postList.find((post) => parent.post === post.id);
+    },
+    author(parent, args, context, info) {
+      return userList.find((user) => parent.author === user.id);
     },
   },
 };
